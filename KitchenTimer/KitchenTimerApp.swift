53b98f2840b586c3c4e07618520259f191f0442d
theo3 <arisstephenson2@gmail.com>
2022-04-05 18:38:43 -0700

    Initial Commit

---
+++ KitchenTimer/KitchenTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenTimerApp.swift
+//  KitchenTimer
+//
+//  Created by Theo on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct KitchenTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
